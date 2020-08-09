0c6d9585800dc1b126436879348d910fb0243965
cyrozap <cyrozap@gmail.com>
2020-08-09 01:08:45 -0500

    Organize the firmware image data in a more hierarchical manner

--- asm_fw.ksy
+++ asm_fw.ksy
@@ -5,43 +5,57 @@ meta:
   license: CC0-1.0
 seq:
   - id: header
-    size: header_len
-  - id: header_checksum
-    type: u1
-  - id: header_crc32
-    type: u4
-  - id: body_len
-    type:
-      switch-on: magic
-      cases:
-        '"2114A_RCFG"': u2
-        '"2214A_RCFG"': u4
+    type: header
   - id: body
-    size: body_len
     type: body
-  - id: footer
-    size: 8
-    type: str
-    encoding: ascii
-  - id: body_checksum
-    type: u1
-  - id: body_crc32
-    type: u4
 types:
+  header:
+    seq:
+      - id: unk0
+        type: u2
+      - id: unk1
+        type: u2
+      - id: len
+        type: u2
+      - id: magic
+        size: 10
+        type: str
+        encoding: ascii
+      - id: data
+        size: len - 16
+      - id: checksum
+        type: u1
+        doc: "A uint8 sum of all the bytes in the header, excluding the checksum and crc32."
+      - id: crc32
+        type: u4
+        doc: "A CRC32 of all the bytes in the header, excluding the checksum and crc32."
   body:
     seq:
-      - id: data
-        size-eos: true
-    instances:
-      version:
-        pos: 0x80
-        size: 6
-instances:
-  magic:
-    pos: 6
-    size: 10
-    type: str
-    encoding: ascii
-  header_len:
-    pos: 4
-    type: u2
+      - id: len
+        type:
+          switch-on: _parent.header.magic
+          cases:
+            '"2114A_RCFG"': u2
+            '"2214A_RCFG"': u4
+      - id: firmware
+        size: len
+        type: firmware
+      - id: magic
+        size: 8
+        type: str
+        encoding: ascii
+      - id: checksum
+        type: u1
+        doc: "A uint8 sum of all the bytes in body.firmware."
+      - id: crc32
+        type: u4
+        doc: "A CRC32 of all the bytes in body.firmware."
+    types:
+      firmware:
+        seq:
+          - id: code
+            size-eos: true
+        instances:
+          version:
+            pos: 0x80
+            size: 6
--- validate_fw.py
+++ validate_fw.py
@@ -19,37 +19,39 @@ def main():
     parser.add_argument("firmware", type=str, help="The ASM1142/ASM2142/ASM3142 firmware binary.")
     args = parser.parse_args()
 
-    fw = asm_fw.AsmFw.from_file(args.firmware)
+    fw_bytes = open(args.firmware, 'rb').read()
+    fw = asm_fw.AsmFw.from_bytes(fw_bytes)
 
-    calc_csum = checksum(fw.header)
-    exp_csum = fw.header_checksum
+    header_bytes = fw_bytes[:fw.header.len]
+    calc_csum = checksum(header_bytes)
+    exp_csum = fw.header.checksum
     if calc_csum != exp_csum:
         print("Error: Invalid header checksum: expected {:#04x}, got: {:#04x}".format(exp_csum, calc_csum), file=sys.stderr)
         sys.exit(1)
     print("Header checksum OK!")
 
-    calc_crc32 = crc32(fw.header)
-    exp_crc32 = fw.header_crc32
+    calc_crc32 = crc32(header_bytes)
+    exp_crc32 = fw.header.crc32
     if calc_crc32 != exp_crc32:
         print("Error: Invalid header crc32: expected {:#010x}, got: {:#010x}".format(exp_crc32, calc_crc32), file=sys.stderr)
         sys.exit(1)
     print("Header CRC32 OK!")
 
-    calc_csum = checksum(fw.body.data)
-    exp_csum = fw.body_checksum
+    calc_csum = checksum(fw.body.firmware.code)
+    exp_csum = fw.body.checksum
     if calc_csum != exp_csum:
         print("Error: Invalid body checksum: expected {:#04x}, got: {:#04x}".format(exp_csum, calc_csum), file=sys.stderr)
         sys.exit(1)
     print("Body checksum OK!")
 
-    calc_crc32 = crc32(fw.body.data)
-    exp_crc32 = fw.body_crc32
+    calc_crc32 = crc32(fw.body.firmware.code)
+    exp_crc32 = fw.body.crc32
     if calc_crc32 != exp_crc32:
         print("Error: Invalid body crc32: expected {:#010x}, got: {:#010x}".format(exp_crc32, calc_crc32), file=sys.stderr)
         sys.exit(1)
     print("Body CRC32 OK!")
 
-    version_string = "{:02X}{:02X}{:02X}_{:02X}_{:02X}_{:02X}".format(*fw.body.version)
+    version_string = "{:02X}{:02X}{:02X}_{:02X}_{:02X}_{:02X}".format(*fw.body.firmware.version)
     print("Firmware version: {}".format(version_string))
 
 
