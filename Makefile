586a95e02d28eba4b3d7d69259f063e277c574ab
cyrozap <cyrozap@gmail.com>
2020-11-26 00:19:17 -0600

    Add memory map and register information for ASM1142

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all: asm_fw.py
 
 %.py: %.ksy
 	kaitai-struct-compiler -t python $<
+
+doc/%.xhtml: data/%.yaml generate_docs.py
+	python3 generate_docs.py -o $@ $<
---
+++ generate_docs.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+import xml.etree.ElementTree as ET
+
+import yaml
+
+
+def validate(doc):
+    unknown_keys = set(doc.keys()).difference(set(['meta', 'xdata', 'registers']))
+    if unknown_keys:
+        print("Error: Invalid keys in document: {}".format(unknown_keys))
+        return False
+
+    xdata = doc.get('xdata', list())
+    if type(xdata) is not list:
+        print("Error: \"xdata\" is not a list.")
+        return False
+
+    for i, region in enumerate(xdata):
+        unknown_keys = set(region.keys()).difference(set(['name', 'start', 'end', 'permissions', 'notes']))
+        if unknown_keys:
+            print("Error: Invalid keys in xdata[{}]: {}".format(i, unknown_keys))
+            return False
+
+    registers = doc.get('registers', dict())
+    if type(registers) is not dict:
+        print("Error: \"registers\" is not a dict.")
+        return False
+
+    for region in ['sfr', 'xdata']:
+        region_registers = registers.get(region, list())
+        if type(region_registers) is not list:
+            print("Error: Register region \"{}\" is not a list.".format(region))
+            return False
+
+        for i, register in enumerate(region_registers):
+            unknown_keys = set(register.keys()).difference(set(['name', 'start', 'end', 'permissions', 'bits', 'notes']))
+            if unknown_keys:
+                print("Error: Invalid keys in registers.{}[{}]: {}".format(region, i, unknown_keys))
+                return False
+
+            bits = register.get('bits', list())
+            if type(bits) is not list:
+                print("Error: Register {}.{}.bits is not a list.".format(region, i))
+                return False
+
+            for b, bit_range in enumerate(bits):
+                unknown_keys = set(bit_range.keys()).difference(set(['name', 'start', 'end', 'permissions', 'notes']))
+                if unknown_keys:
+                    print("Error: Invalid keys in registers.{}[{}].bits[{}]: {}".format(region, i, b, unknown_keys))
+                    return False
+
+    return True
+
+def gen_xhtml(doc):
+    html = ET.Element('html', {
+        'xmlns': "http://www.w3.org/1999/xhtml",
+        'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+        'xsi:schemaLocation': "http://www.w3.org/MarkUp/SCHEMA/xhtml11.xsd",
+        'xml:lang': "en",
+    })
+    head = ET.SubElement(html, 'head')
+    body = ET.SubElement(html, 'body')
+
+    meta = doc.get('meta', dict())
+    chip = meta.get('chip', "UNKNOWN")
+
+    title = ET.SubElement(head, 'title')
+    title.text = "{} Memory Map and Register Manual".format(chip)
+
+    heading = ET.SubElement(body, 'h1')
+    heading.text = title.text
+
+    ET.SubElement(body, 'h2').text = "XDATA Memory Map"
+    xdata_memory_map = ET.SubElement(body, 'table')
+    xdata_memory_map_header = ET.SubElement(xdata_memory_map, 'tr')
+    for header in ["Start", "End", "Size", "Name", "Permissions", "Notes"]:
+        ET.SubElement(xdata_memory_map_header, 'th').text = header
+
+    xdata = doc.get('xdata', list())
+    for region in xdata:
+        tr = ET.SubElement(xdata_memory_map, 'tr')
+        start = region.get('start')
+        start_text = ""
+        if start is not None:
+            start_text = "0x{:04X}".format(start)
+        ET.SubElement(tr, 'td').text = start_text
+        end = region.get('end')
+        end_text = ""
+        if end is not None:
+            end_text = "0x{:04X}".format(end)
+        ET.SubElement(tr, 'td').text = end_text
+        size_text = ""
+        if start is not None and end is not None:
+            size_text = "0x{:04X}".format(end + 1 - start)
+        ET.SubElement(tr, 'td').text = size_text
+        ET.SubElement(tr, 'td').text = region.get('name', "")
+        ET.SubElement(tr, 'td').text = region.get('permissions', "")
+        ET.SubElement(tr, 'td').text = " ".join(region.get('notes', "").strip('\n').split('\n'))
+
+    ET.SubElement(body, 'h2').text = "Register Map"
+    register_regions = doc.get('registers', dict())
+    for region_name, region_registers in register_regions.items():
+        ET.SubElement(body, 'h3').text = "{} Region Registers".format(region_name.upper())
+        for register in region_registers:
+            reg_name = register.get('name', "")
+            addr_format = "0x{:04X}"
+            if region_name == "sfr":
+                addr_format = "0x{:02X}"
+            start = register.get('start')
+            addr_string = ""
+            if start is not None:
+                addr_string = addr_format.format(start)
+            ET.SubElement(body, 'h4').text = "{}: {}".format(addr_string, reg_name)
+            end = register.get('end')
+            size = 0
+            if start is not None and end is not None:
+                size = end + 1 - start
+            max_bits = size * 8
+            ET.SubElement(body, 'p').text = "Size: {} byte{}".format(size, "s" if size > 1 else "")
+            ET.SubElement(body, 'p').text = " ".join(register.get('notes', "").strip('\n').split('\n'))
+            if max_bits > 64:
+                continue
+            bit_table = ET.SubElement(body, 'table')
+            bits = register.get('bits')
+            if bits is None:
+                bits = [{
+                    'name': reg_name,
+                    'start': 0,
+                    'end': max_bits - 1,
+                    'permissions': register.get('permissions', ""),
+                }]
+            unused_bits = set(range(max_bits))
+            bit_spans = dict()
+            for bit_range in bits:
+                bit_start = bit_range.get('start')
+                bit_end = bit_range.get('end')
+                if bit_start is None or bit_end is None:
+                    continue
+                unused_bits = unused_bits.difference(set(range(bit_start, bit_end + 1)))
+                bit_len = bit_end + 1 - bit_start
+                start_bit_block = bit_start // 16
+                end_bit_block = bit_end // 16
+                if end_bit_block > start_bit_block:
+                    for block in range(start_bit_block, end_bit_block + 1):
+                        if block == end_bit_block:
+                            upper_len = bit_end + 1 - block*16
+                            bit_spans[bit_end] = {
+                                'name': bit_range.get('name', ""),
+                                'permissions': bit_range.get('permissions', ""),
+                                'span': upper_len,
+                            }
+                        elif block == start_bit_block:
+                            lower_len = block*16+16 - bit_start
+                            bit_spans[block*16+16-1] = {
+                                'name': bit_range.get('name', ""),
+                                'permissions': bit_range.get('permissions', ""),
+                                'span': lower_len,
+                            }
+                        else:
+                            bit_spans[block*16+16-1] = {
+                                'name': bit_range.get('name', ""),
+                                'permissions': bit_range.get('permissions', ""),
+                                'span': 16,
+                            }
+                else:
+                    bit_spans[bit_end] = {
+                        'name': bit_range.get('name', ""),
+                        'permissions': bit_range.get('permissions', ""),
+                        'span': bit_len,
+                    }
+            if max_bits > 32:
+                bit_table_upper_header = ET.SubElement(bit_table, 'tr')
+                for bit in range(48, 64)[::-1]:
+                    ET.SubElement(bit_table_upper_header, 'th').text = str(bit)
+                bit_table_upper_perms = ET.SubElement(bit_table, 'tr')
+                for bit in range(48, 64)[::-1]:
+                    if bit in bit_spans.keys():
+                        span = bit_spans[bit]
+                        bit_element = ET.SubElement(bit_table_upper_perms, 'td')
+                        bit_element.text = span['permissions']
+                        colspan = span['span']
+                        if colspan > 1:
+                            bit_element.set('colspan', str(colspan))
+                    elif bit in unused_bits:
+                        ET.SubElement(bit_table_upper_perms, 'td')
+                bit_table_upper_data = ET.SubElement(bit_table, 'tr')
+                for bit in range(48, 64)[::-1]:
+                    if bit in bit_spans.keys():
+                        span = bit_spans[bit]
+                        bit_element = ET.SubElement(bit_table_upper_data, 'td')
+                        bit_element.text = span['name']
+                        colspan = span['span']
+                        if colspan > 1:
+                            bit_element.set('colspan', str(colspan))
+                    elif bit in unused_bits:
+                        ET.SubElement(bit_table_upper_data, 'td')
+                bit_table_lower_header = ET.SubElement(bit_table, 'tr')
+                for bit in range(32, 48)[::-1]:
+                    ET.SubElement(bit_table_lower_header, 'th').text = str(bit)
+                bit_table_lower_perms = ET.SubElement(bit_table, 'tr')
+                for bit in range(32, 48)[::-1]:
+                    if bit in bit_spans.keys():
+                        span = bit_spans[bit]
+                        bit_element = ET.SubElement(bit_table_lower_perms, 'td')
+                        bit_element.text = span['permissions']
+                        colspan = span['span']
+                        if colspan > 1:
+                            bit_element.set('colspan', str(colspan))
+                    elif bit in unused_bits:
+                        ET.SubElement(bit_table_lower_perms, 'td')
+                bit_table_lower_data = ET.SubElement(bit_table, 'tr')
+                for bit in range(32, 48)[::-1]:
+                    if bit in bit_spans.keys():
+                        span = bit_spans[bit]
+                        bit_element = ET.SubElement(bit_table_lower_data, 'td')
+                        bit_element.text = span['name']
+                        colspan = span['span']
+                        if colspan > 1:
+                            bit_element.set('colspan', str(colspan))
+                    elif bit in unused_bits:
+                        ET.SubElement(bit_table_lower_data, 'td')
+            if max_bits > 16:
+                bit_table_upper_header = ET.SubElement(bit_table, 'tr')
+                for bit in range(16, 32)[::-1]:
+                    ET.SubElement(bit_table_upper_header, 'th').text = str(bit)
+                bit_table_upper_perms = ET.SubElement(bit_table, 'tr')
+                for bit in range(16, 32)[::-1]:
+                    if bit in bit_spans.keys():
+                        span = bit_spans[bit]
+                        bit_element = ET.SubElement(bit_table_upper_perms, 'td')
+                        bit_element.text = span['permissions']
+                        colspan = span['span']
+                        if colspan > 1:
+                            bit_element.set('colspan', str(colspan))
+                    elif bit in unused_bits:
+                        ET.SubElement(bit_table_upper_perms, 'td')
+                bit_table_upper_data = ET.SubElement(bit_table, 'tr')
+                for bit in range(16, 32)[::-1]:
+                    if bit in bit_spans.keys():
+                        span = bit_spans[bit]
+                        bit_element = ET.SubElement(bit_table_upper_data, 'td')
+                        bit_element.text = span['name']
+                        colspan = span['span']
+                        if colspan > 1:
+                            bit_element.set('colspan', str(colspan))
+                    elif bit in unused_bits:
+                        ET.SubElement(bit_table_upper_data, 'td')
+            bit_table_lower_header = ET.SubElement(bit_table, 'tr')
+            for bit in range(0, min(16, max_bits))[::-1]:
+                ET.SubElement(bit_table_lower_header, 'th').text = str(bit)
+            bit_table_lower_perms = ET.SubElement(bit_table, 'tr')
+            for bit in range(0, min(16, max_bits))[::-1]:
+                if bit in bit_spans.keys():
+                    span = bit_spans[bit]
+                    bit_element = ET.SubElement(bit_table_lower_perms, 'td')
+                    bit_element.text = span['permissions']
+                    colspan = span['span']
+                    if colspan > 1:
+                        bit_element.set('colspan', str(colspan))
+                elif bit in unused_bits:
+                    ET.SubElement(bit_table_lower_perms, 'td')
+            bit_table_lower_data = ET.SubElement(bit_table, 'tr')
+            for bit in range(0, min(16, max_bits))[::-1]:
+                if bit in bit_spans.keys():
+                    span = bit_spans[bit]
+                    bit_element = ET.SubElement(bit_table_lower_data, 'td')
+                    bit_element.text = span['name']
+                    colspan = span['span']
+                    if colspan > 1:
+                        bit_element.set('colspan', str(colspan))
+                elif bit in unused_bits:
+                    ET.SubElement(bit_table_lower_data, 'td')
+
+            actual_bits = register.get('bits', list())
+            for bit_range in actual_bits[::-1]:
+                start_bit = bit_range.get('start')
+                end_bit = bit_range.get('end')
+                if start_bit is None or end_bit is None:
+                    continue
+                bit = "Bit [{}]".format(start_bit)
+                if end_bit != start_bit:
+                    bit = "Bits [{}:{}]".format(end_bit, start_bit)
+                bit_notes = " ".join(bit_range.get('notes', "").strip('\n').split('\n'))
+                ET.SubElement(body, 'p').text = "{}, {}: {}".format(bit, bit_range.get('name', ""), bit_notes)
+
+    return ET.tostring(html, encoding='utf-8', xml_declaration=False)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", type=str, default="regs.xhtml", help="The output file.")
+    parser.add_argument("input", type=str, help="The input YAML register definition file.")
+    args = parser.parse_args()
+
+    doc = yaml.safe_load(open(args.input, 'r'))
+
+    doc_valid = validate(doc)
+    if not doc_valid:
+        print("Error: Document \"{}\" invalid.".format(args.input))
+        return 1
+
+    xhtml = gen_xhtml(doc)
+    output = open(args.output, 'wb')
+    output.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+    output.write(b"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n")
+    output.write(xhtml)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
