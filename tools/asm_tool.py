b09db47e6ef7193f32e000a2c56bc06639766b55
cyrozap <cyrozap@gmail.com>
2021-09-18 01:09:38 -0500

    Add functions for direct firmware loading and execution

--- tools/asm_tool.py
+++ tools/asm_tool.py
@@ -82,14 +82,17 @@ class AsmDev:
         (0x1b21, 0x1042): {
             'name': "ASM1042",
             'hw_mmio': False,
+            'hw_code': False,
         },
         (0x1b21, 0x1142): {
             'name': "ASM1042A",
             'hw_mmio': True,
+            'hw_code': True,
         },
         (0x1b21, 0x1242): {
             'name': "ASM1142",
             'hw_mmio': True,
+            'hw_code': True,
         },
         (0x1b21, 0x2142): {
             'name': "ASM2142/ASM3142",
@@ -101,9 +104,15 @@ class AsmDev:
         },
     }
 
+    CODE_RAM_ADDR = 0xE2
+    CODE_RAM_DATA_LOWER_BANK_DATA = 0xE4
+    CODE_RAM_DATA_UPPER_BANK_DATA = 0xE6
     MMIO_ACCESS_ADDR = 0xE8
     MMIO_ACCESS_WRITE_DATA = 0xEA
     MMIO_ACCESS_READ_DATA = 0xEB
+
+    CPU_MODE_NEXT_64K = 0xF340
+    CPU_EXEC_CTRL_64K = 0xF342
 
     def __init__(self, dbsf: str, debug: bool = False, verbose: bool = False):
         self.debug = debug
@@ -118,6 +127,68 @@ class AsmDev:
         self.chip = self.ids_map[(vid, did)]
         self.name = self.chip['name']
         self.hw_mmio = self.chip.get('hw_mmio', False)
+        self.hw_code = self.chip.get('hw_code', False)
+
+    def hw_code_write(self, addr: int, code: bytes):
+        if not self.hw_code:
+            raise ValueError("{} is not capable of hardware CODE access.".format(self.name))
+
+        if not (addr >= 0 and addr <= 0xfffe):
+            raise ValueError("Invalid address, must be >= 0x0000 and <= 0xFFFE: {:#x}".format(addr))
+
+        if addr % 2 != 0:
+            raise ValueError("Invalid address, must be 2-byte aligned: 0x{:04x}".format(addr))
+
+        if len(code) > 0x10000:
+            raise ValueError("Invalid code length, must be less than 0x10000: {:#x}".format(len(code)))
+
+        if len(code) % 2 != 0:
+            raise ValueError("Invalid code length, must be a multiple of 2: 0x{:04x}".format(len(code)))
+
+        # Enable hardware CODE write access.
+        reg_F343 = self.hw_mmio_reg_read(0xF343, 1)
+        self.hw_mmio_reg_write(0xF343, 1, reg_F343 | (1 << 1), confirm=True)
+
+        # Write to CODE memory.
+        for i, (word,) in enumerate(struct.iter_unpack('<H', code)):
+            offset_addr = addr + (i * 2)
+
+            reg = self.CODE_RAM_DATA_LOWER_BANK_DATA
+            if (offset_addr & (1 << 15)):
+                reg = self.CODE_RAM_DATA_UPPER_BANK_DATA
+
+            masked_addr = offset_addr & 0x7ffe
+            self.pci.config_reg_write(self.CODE_RAM_ADDR, 2, masked_addr)
+            while self.pci.config_reg_read(self.CODE_RAM_ADDR, 2) != masked_addr:
+                time.sleep(0.001)
+
+            self.pci.config_reg_write(reg, 2, word)
+
+        # Disable hardware CODE write access.
+        reg_F343 = self.hw_mmio_reg_read(0xF343, 1)
+        self.hw_mmio_reg_write(0xF343, 1, reg_F343 & ~(1 << 1), confirm=True)
+
+    def hw_code_load_exec(self, code: bytes, half_speed: bool = True):
+        if not self.hw_code:
+            raise ValueError("{} is not capable of hardware CODE access.".format(self.name))
+
+        if len(code) > 0x10000:
+            raise ValueError("Invalid code length, must be less than 0x10000: {:#x}".format(len(code)))
+
+        if len(code) % 2 != 0:
+            raise ValueError("Invalid code length, must be a multiple of 2: 0x{:04x}".format(len(code)))
+
+        # Halt the CPU.
+        self.hw_mmio_reg_write(self.CPU_EXEC_CTRL_64K, 1, 1 << 1)
+
+        # Write the program to CODE RAM.
+        self.hw_code_write(0x0000, code)
+
+        # Configure CPU to boot from CODE RAM.
+        self.hw_mmio_reg_write(self.CPU_MODE_NEXT_64K, 1, (1 << 1) | (1 if half_speed else 0))
+
+        # Release the CPU from reset.
+        self.hw_mmio_reg_write(self.CPU_EXEC_CTRL_64K, 1, 0)
 
     def hw_mmio_reg_read(self, addr: int, width: int):
         if not self.hw_mmio:
@@ -144,7 +215,7 @@ class AsmDev:
 
         return value
 
-    def hw_mmio_reg_write(self, addr: int, width: int, value: int):
+    def hw_mmio_reg_write(self, addr: int, width: int, value: int, confirm: bool = False):
         if not self.hw_mmio:
             raise ValueError("{} is not capable of hardware MMIO access.".format(self.name))
 
@@ -161,6 +232,22 @@ class AsmDev:
                 time.sleep(0.001)
             byte_value = (value >> (8 * i)) & 0xff
             self.pci.config_reg_write(self.MMIO_ACCESS_WRITE_DATA, 1, byte_value)
+
+        # If "confirm" is set, repeatedly read the register until its contents match the value written.
+        if confirm:
+            while True:
+                actual_value = 0
+                for i in range(width):
+                    byte_addr = (addr + i) & 0xffff
+                    self.pci.config_reg_write(self.MMIO_ACCESS_ADDR, 2, byte_addr)
+                    while self.pci.config_reg_read(self.MMIO_ACCESS_ADDR, 2) != byte_addr:
+                        time.sleep(0.001)
+                    byte_actual_value = self.pci.config_reg_read(self.MMIO_ACCESS_READ_DATA, 1)
+
+                    actual_value |= byte_actual_value << (8 * i)
+
+                if actual_value == value:
+                    break
 
 
 def main():
