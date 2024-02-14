a8304fe123624ce2d2ca8e47c7d4f3f02ffb6191
cyrozap <cyrozap@gmail.com>
2024-02-14 00:59:40 -0600

    Print chip name for BROM

--- tools/validate_brom.py
+++ tools/validate_brom.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # validate_brom.py - A tool to validate that a boot ROM was dumped properly.
-# Copyright (C) 2021  Forest Crossman <cyrozap@gmail.com>
+# Copyright (C) 2021, 2024  Forest Crossman <cyrozap@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,6 +24,16 @@ import sys
 from zlib import crc32
 
 
+CHIPS = {
+    bytes(8): "ASM1042",
+    b"2104B_FW": "ASM1042A",
+    b"2114A_FW": "ASM1142",
+    b"2214A_FW": "ASM2142/ASM3142",
+    b"2324A_FW": "ASM3242",
+    b"3328A_FW": "ASM3283/Prom21",
+}
+
+
 def validate_crc32(data, expected):
     calc_crc32 = crc32(data)
     exp_crc32 = expected
@@ -36,6 +46,8 @@ def main():
     args = parser.parse_args()
 
     fw_bytes = open(args.firmware, 'rb').read()
+    chip_magic = fw_bytes[0x87:0x87+8]
+    chip_name = CHIPS.get(chip_magic, "Unknown magic: {}".format(chip_magic))
 
     for size in (0x8000, 0x10000):
         expected = struct.unpack_from('<I', fw_bytes, size-4)[0]
@@ -45,7 +57,7 @@ def main():
 
             version_bytes = fw_bytes[0x80:0x80+6]
             version_string = "{:02X}{:02X}{:02X}_{:02X}_{:02X}_{:02X}".format(*version_bytes)
-            print("BROM CRC32 OK! BROM version: {}, BROM size: {} bytes".format(version_string, size))
+            print("BROM CRC32 OK! Chip name: {}, BROM version: {}, BROM size: {} bytes".format(chip_name, version_string, size))
 
             return 0
         except ValueError:
