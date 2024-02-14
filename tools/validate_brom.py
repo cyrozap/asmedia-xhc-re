339b32abd539eb3da3476a45b2766ed05322c7f9
cyrozap <cyrozap@gmail.com>
2024-02-14 02:08:00 -0600

    Include hashes in CSV output

--- tools/validate_brom.py
+++ tools/validate_brom.py
@@ -23,6 +23,7 @@ import csv
 import io
 import struct
 import sys
+from hashlib import md5, sha1, sha256, sha512, blake2b
 from zlib import crc32
 
 
@@ -61,9 +62,13 @@ def main():
             version_bytes = fw_bytes[0x80:0x80+6]
             version_string = "{:02X}{:02X}{:02X}_{:02X}_{:02X}_{:02X}".format(*version_bytes)
             if args.csv:
+                hashes = []
+                for hash_alg in (md5, sha1, sha256, sha512, blake2b):
+                    # Calculate hashes on the 32 kB / 64 kB BROM (includes the CRC-32)
+                    hashes.append(hash_alg(fw_bytes[:size], usedforsecurity=False).hexdigest())
                 with io.StringIO(newline="") as csvfile:
                     csvwriter = csv.writer(csvfile)
-                    csvwriter.writerow([chip_name, version_string, size, "0x{:08X}".format(expected)])
+                    csvwriter.writerow([chip_name, version_string, size, "0x{:08X}".format(expected)] + hashes)
                     print(csvfile.getvalue().rstrip())
             else:
                 print("BROM CRC-32 OK! Chip name: {}, BROM version: {}, BROM size: {} bytes, BROM CRC-32: 0x{:08X}".format(
