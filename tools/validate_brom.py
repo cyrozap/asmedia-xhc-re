58ea4e22e4b0ec48cef580b10f57c47c5d3d780f
cyrozap <cyrozap@gmail.com>
2025-05-31 16:12:25 -0500

    Add type hints to validate_brom.py

--- tools/validate_brom.py
+++ tools/validate_brom.py
@@ -27,7 +27,7 @@ from hashlib import md5, sha1, sha256, sha512, blake2b
 from zlib import crc32
 
 
-CHIPS = {
+CHIPS: dict[bytes, str] = {
     bytes(8): "ASM1042",
     b"2104B_FW": "ASM1042A",
     b"2114A_FW": "ASM1142",
@@ -40,32 +40,32 @@ CHIPS = {
 }
 
 
-def validate_crc32(data, expected) -> None:
-    calc_crc32 = crc32(data)
-    exp_crc32 = expected
+def validate_crc32(data: bytes, expected: int) -> None:
+    calc_crc32: int = crc32(data)
+    exp_crc32: int = expected
     if calc_crc32 != exp_crc32:
         raise ValueError("Invalid CRC-32: expected {:#010x}, got: {:#010x}".format(exp_crc32, calc_crc32))
 
 def main() -> int:
-    parser = argparse.ArgumentParser()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
     parser.add_argument("-c", "--csv", default=False, action="store_true", help="Output in CSV format.")
     parser.add_argument("firmware", type=str, help="The ASMedia USB 3 host controller boot ROM binary.")
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
-    fw_bytes = open(args.firmware, 'rb').read()
-    chip_magic = fw_bytes[0x87:0x87+8]
-    chip_name = CHIPS.get(chip_magic, "Unknown magic: {!r}".format(chip_magic))
+    fw_bytes: bytes = open(args.firmware, 'rb').read()
+    chip_magic: bytes = fw_bytes[0x87:0x87+8]
+    chip_name: str = CHIPS.get(chip_magic, "Unknown magic: {!r}".format(chip_magic))
 
     for size in (0x8000, 0x10000):
-        expected = struct.unpack_from('<I', fw_bytes, size-4)[0]
+        expected: int = struct.unpack_from('<I', fw_bytes, size-4)[0]
 
         try:
             validate_crc32(fw_bytes[:size-4], expected)
 
-            version_bytes = fw_bytes[0x80:0x80+6]
-            version_string = "{:02X}{:02X}{:02X}_{:02X}_{:02X}_{:02X}".format(*version_bytes)
+            version_bytes: bytes = fw_bytes[0x80:0x80+6]
+            version_string: str = "{:02X}{:02X}{:02X}_{:02X}_{:02X}_{:02X}".format(*version_bytes)
             if args.csv:
-                hashes = []
+                hashes: list[str] = []
                 for hash_alg in (md5, sha1, sha256, sha512, blake2b):
                     # Calculate hashes on the 32 kB / 64 kB BROM (includes the CRC-32)
                     hashes.append(hash_alg(fw_bytes[:size], usedforsecurity=False).hexdigest())
